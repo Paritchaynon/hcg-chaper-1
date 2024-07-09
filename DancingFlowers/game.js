d66ad29fe3eccc4a52aaa5d3ffba9577bb1241c5
Saiko <paritchayanon_chaita@cmu.ac.th>
2024-07-09 09:23:34 +0700

    Add 9-7-2024

---
+++ DancingFlowers/game.js
@@ -0,0 +1,15 @@
+﻿class Game{
+    constructor() {
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        
+        this.sfx = new SFX({
+            context: this.audioContext,
+            src:{mp3:"quick_swish.mp3", webm:"quick_swish.webm"},
+            loop: false
+        });
+        
+        const btn1 = document.getElementById("playSndBtn");
+        const game = this;
+        btn1.addEventListener('click', function(){game.sfx.play();})
+    }
+}
